0dcc0b783734711f7952b2219964b2e16f9ffb66
SATHYAMOORTHY TS <sathyamoortthyts0105@gmail.com>
2025-01-02 12:47:58 +0530

    used socket.io to connect multiple clients in one server

--- ws-1/index.js
+++ ws-1/index.js
@@ -1,10 +1,23 @@
-const ws = require('ws');
-const server = new ws.Server({port : '3000'})
+import { createServer } from "http";
+import { Server } from "socket.io"
+const httpServer = createServer()
 
-server.on('connection',socket =>{
-    socket.on('message',message =>{
-        const b = Buffer.from(message)
-        console.log(b.toString())
-        socket.send(`${message}`)
+const io = new Server(httpServer , {
+     cors : {
+        origin : process.env.NODE_ENV === "production" ? false :[ "http://localhost:3000",'http://127.0.0.1:5500']
+     }
+})
+
+io.on('connection',socket =>{
+   console.log(`User ${socket.id} connected`)
+
+    socket.on('message',data =>{
+       console.log(data)
+       io.emit('message' , `${socket.id} : ${data}` )
     })
+    socket.
+})
+
+httpServer.listen(3500, ()=>{
+   console.log('listening to 3500')
 })
