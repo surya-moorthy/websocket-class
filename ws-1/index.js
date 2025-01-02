67279fe66a9533bb434d6ceed1edd5839d84c493
SATHYAMOORTHY TS <sathyamoortthyts0105@gmail.com>
2025-01-02 13:11:10 +0530

    something

--- ws-1/index.js
+++ ws-1/index.js
@@ -1,23 +1,21 @@
-import { createServer } from "http";
+import { createServer } from "http"
 import { Server } from "socket.io"
 const httpServer = createServer()
-
-const io = new Server(httpServer , {
-     cors : {
-        origin : process.env.NODE_ENV === "production" ? false :[ "http://localhost:3000",'http://127.0.0.1:5500']
-     }
-})
+const io = new Server(httpServer, {
+    cors : {
+        origin : process.env.NODE_ENV === "production" ? false : ["http://localhost:3000","http://127.0.0.1:5500"]
+    }
+});
 
 io.on('connection',socket =>{
-   console.log(`User ${socket.id} connected`)
+    console.log(` User ${socket.id} is connected`)
 
-    socket.on('message',data =>{
-       console.log(data)
-       io.emit('message' , `${socket.id} : ${data}` )
-    })
-    socket.
+    socket.on('message', data =>{
+        console.log(data);
+        io.emit('message', `${socket.id} : ${data}` )
+    } )
 })
 
 httpServer.listen(3500, ()=>{
-   console.log('listening to 3500')
+    console.log("Port running in 3500")
 })
