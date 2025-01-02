3317809d3cb5449ecac25ce87fa8fba3af0b337d
SATHYAMOORTHY TS <sathyamoortthyts0105@gmail.com>
2025-01-02 11:42:21 +0530

    made some changes in ws server

--- ws-1/index.js
+++ ws-1/index.js
@@ -5,6 +5,6 @@ server.on('connection',socket =>{
     socket.on('message',message =>{
         const b = Buffer.from(message)
         console.log(b.toString())
-        socket.send(`${b.toString()}`)
+        socket.send(`${message}`)
     })
 })
